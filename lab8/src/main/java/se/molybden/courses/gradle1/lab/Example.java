37da0c553795e4c1965a414e65cc1b3bb297e496
jocce.nilsson@gmail.com <Joachim.Nilsson@molybden.se>
2014-03-16 10:10:56 +0100

    Restructured from 1.0 feedback

--- lab8/src/main/java/se/molybden/courses/gradle1/lab/Example.java
+++ lab8/src/main/java/se/molybden/courses/gradle1/lab/Example.java
@@ -3,6 +3,6 @@ package se.molybden.courses.gradle1.lab;
 
 public class Example {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello "+args[0]);
 	}
 }
